0c3dde4297f479d57cd7e79278af8b3027ec5cfb
gabrielle.tsai <linda41436@gmail.com>
2024-04-18 18:52:44 +0800

    Add Grid.module.css, Steps.module.css, modified App.js style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import StepProgress from './components/Step/StepProgress'
 import { Step1, Step2, Step3 } from './components/Step/Steps'
 import ProgressControl from './components/Step/ProgressControl'
 
+function Main({children}) {
+  return (
+    <div id="main-app">
+      {children}
+    </div>
+  )
+}
+
+function StepsPanel({children}) {
+  return (
+    <div className='stepsPanel'>
+      {children}
+    </div>
+  )
+}
+
 function App() {
 
   return (
-    <>
-      <StepProgress />
-      <Step1 />
-      <ProgressControl />
-    </>
+    <Main>
+      <StepsPanel>
+        <StepProgress />
+        <Step1 />
+        <ProgressControl />
+      </StepsPanel>
+    </Main>
   )
 }
 
