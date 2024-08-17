76bdaf0fb8bc8918838dcc05cee73934f2c27eb5
Gabrielle Tsai <linda41436@gmail.com>
2024-08-18 00:46:34 +0800

    調整 build 內容

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  }
-})
+  build: {
+    outDir: "dist", // 指定打包後的輸出目錄
+    rollupOptions: {
+      // 自定義 Rollup 設置
+    },
+  },
+});
